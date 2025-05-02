bac9699abf55f2745f4b49958f5158d5f87ade43
Blane Townsend <blane.townsend@gmail.com>
2025-05-02 15:55:14 -0500

    Changed the SSID

--- src/mavesp8266_parameters.cpp
+++ src/mavesp8266_parameters.cpp
@@ -182,7 +182,7 @@ MavESP8266Parameters::resetToDefaults()
 {
     _sw_version        = MAVESP8266_VERSION;
     _debug_enabled     = 0;
-    _wifi_mode         = DEFAULT_WIFI_MODE;
+    _wifi_mode         = 1; //DEFAULT_WIFI_MODE - used to be AP now it's station
     _wifi_channel      = DEFAULT_WIFI_CHANNEL;
     _wifi_udp_hport    = DEFAULT_UDP_HPORT;
     _wifi_udp_cport    = DEFAULT_UDP_CPORT;
